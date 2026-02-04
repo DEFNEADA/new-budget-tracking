5087575a6255142f8c9890c29f6b4de337a7aad6
Defne Ada <defne.ada@sans-technology.com>
2026-02-04 16:02:57 +0300

    DA-001 : change description field from text input to select menu.

--- new/assets/js/calculation.js
+++ new/assets/js/calculation.js
@@ -1,27 +1,23 @@
-
-
 export function formatCurrency(amount) {
     return new Intl.NumberFormat('tr-TR', {
         style: 'currency',
         currency: 'TRY',
-        minimumFractionDigits: 2
+        minimumFractionDigits: 2,
     }).format(amount);
 }
 
-export function calculations(){
-        let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
-    
+export function calculations() {
+    let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
     const totalincome = transactions
-    .filter((item) => item.type == 'income')
-    .reduce((sum, item) => sum + Number(item.amount), 0);
+        .filter((item) => item.type == 'income')
+        .reduce((sum, item) => sum + Number(item.amount), 0);
 
     const totalexpense = transactions
-    .filter((item) => item.type == 'expense')
-    .reduce((sum, item) => sum+ Number(item.amount), 0);
-      
-     const totalbalance=totalincome-totalexpense;
+        .filter((item) => item.type == 'expense')
+        .reduce((sum, item) => sum + Number(item.amount), 0);
 
-     return { totalincome, totalexpense, totalbalance };
+    const totalbalance = totalincome - totalexpense;
 
-     
+    return { totalincome, totalexpense, totalbalance };
 }
--- new/assets/js/render.js
+++ new/assets/js/render.js
@@ -1,45 +1,57 @@
-import { calculations, formatCurrency} from "./calculation.js"; 
+import { calculations, formatCurrency } from './calculation.js';
 export function renderTransactions() {
-  let transactionlist = document.getElementById('transactionList');
+    let transactionlist = document.getElementById('transactionList');
 
-  let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
-  const { totalbalance, totalincome, totalexpense } = calculations();
-  if (!transactionlist) return;
-  transactionlist.innerHTML = '';
+    const translations = {
+        food: 'Yemek',
+        rent: 'Kira',
+        bills: 'Faturalar',
+        transport: 'Ulaşım',
+        shopping: 'Alışveriş',
+        salary: 'Maaş',
+        education: 'Eğitim',
+        health: 'Sağlık',
+        entertainment: 'Eğlence',
+        other: 'Diğer',
+    };
 
-  const fragment = document.createDocumentFragment();
+    let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    const { totalbalance, totalincome, totalexpense } = calculations();
+    if (!transactionlist) return;
+    transactionlist.innerHTML = '';
 
-  transactions.forEach(function (transaction) {
-    const newtransaction = document.createElement('div');
-    newtransaction.setAttribute('data-id', transaction.id);
-    newtransaction.classList.add('new-transaction');
+    const fragment = document.createDocumentFragment();
 
+    transactions.map(function (transaction) {
+        const newtransaction = document.createElement('div');
+        newtransaction.setAttribute('data-id', transaction.id);
+        newtransaction.classList.add('new-transaction');
 
-    if (transaction.type == 'income') {
-      newtransaction.classList.add('income');
-    } else {
-      newtransaction.classList.add('expense');
-    }
+        if (transaction.type == 'income') {
+            newtransaction.classList.add('income');
+        } else {
+            newtransaction.classList.add('expense');
+        }
 
-    newtransaction.innerHTML = `
+        newtransaction.innerHTML = `
     <span class="transaction-text"></span>
     <span class="buttons">
         <button class='edit-btn'><i class="fa fa-pencil" aria-hidden="true"></i></button>
         <button class='delete-btn'>&times;</button>
     </span>
 `;
-   newtransaction.querySelector('.transaction-text').textContent = 
-    `${transaction.description} : ${formatCurrency(transaction.amount)}`;
-     fragment.appendChild(newtransaction);
-  });
+        newtransaction.querySelector('.transaction-text').textContent =
+            `${translations[transaction.description]} : ${formatCurrency(transaction.amount)}`;
+        fragment.appendChild(newtransaction);
+    });
 
-   transactionlist.appendChild(fragment);
+    transactionlist.appendChild(fragment);
 
     const totalCard = document.getElementById('totalBalance');
     const incomeCard = document.getElementById('incomeCard');
     const expenseCard = document.getElementById('expenseCard');
 
-      totalCard.querySelector('span').textContent = `${formatCurrency(totalbalance)}`;
-      incomeCard.querySelector('span').textContent = `${formatCurrency(totalincome)}`;
-      expenseCard.querySelector('span').textContent = `${formatCurrency(totalexpense)}`;
+    totalCard.querySelector('span').textContent = `${formatCurrency(totalbalance)}`;
+    incomeCard.querySelector('span').textContent = `${formatCurrency(totalincome)}`;
+    expenseCard.querySelector('span').textContent = `${formatCurrency(totalexpense)}`;
 }
